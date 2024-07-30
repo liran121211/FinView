562277c007332852f9f890b4178b6b275ebef87b
Liran.S <63954850+liran121211@users.noreply.github.com>
2024-07-30 18:19:28 +0300

    Update settings.py

--- FinWeb/Web/settings.py
+++ FinWeb/Web/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8*4vzaxkucf%df#zioekp!mk6qch%88_k24y1!@$!y^&hjpbu%'
+SECRET_KEY = 'HIDDEN'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
